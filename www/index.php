3378e68cfa6e8fa3ace1fcc76dfd6509624792a7
Nigel <nigelpoulton@hotmail.com>
2015-08-21 22:36:06 +0100

    commit 3

--- www/index.php
+++ www/index.php
@@ -15,39 +15,7 @@
 	</style>
 </head>
 <body>
-	<img id="logo" src="docker-logo.png" />
+	<img id="logo" src="logo-white.png" />
 	<h1><?php echo "Hello Docker Hub Automated Builds!"; ?></h1>
-	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
-	<?php
-	$links = [];
-	foreach($_ENV as $key => $value) {
-		if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
-			$links[] = [
-				"name" => $matches[1],
-				"port" => $matches[2],
-				"proto" => $matches[3],
-				"value" => $value
-			];
-		}
-	}
-	if($links) {
-	?>
-		<h3>Links found</h3>
-		<?php
-		foreach($links as $link) {
-			?>
-			<b><?php echo $link["name"]; ?></b> listening in <?php echo $link["port"]+"/"+$link["proto"]; ?> available at <?php echo $link["value"]; ?><br />
-			<?php
-		}
-		?>
-	<?php
-	}
-
-	if($_ENV["TUTUM_AUTH"]) {
-		?>
-		<h3>I have Tutum API powers!</h3>
-		<?php
-	}
-	?>
 </body>
 </html>
